f7c28019deef8f70b7df080c9c4a14b759380f04
oleksandra <104913146+leksi-s@users.noreply.github.com>
2022-05-04 17:21:15 +0300

    Add files via upload

---
+++ Region.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лаба2._1___
+{
+    enum Regions
+    {
+        Київська = 1, Львівська, Івано_Франківська, Тернопільська,
+        Вінницька, Дніпровська, Харківська, Хмельницька, Рівненська,
+        Полтавська, Волинська, Одеська, Чернівецька, Запорізька, Черкаська,
+        Кіровоградська, Житомирська, Ужгородська, Кімолаївська, Сумська, Херсонська,
+        Чернігівська, Донецька, Луганська, Сімферопольська
+    }
+    internal class Region
+    {
+        public int NumberOfRegions()
+        {
+            int number = Enum.GetNames(typeof(Regions)).Length; 
+            return number;
+        }
+        public void CallUp()
+        {
+            Console.WriteLine($"Ви викликали метод Region.");
+        }
+    }
+}
